1164a751e221d0430381e3945ae89dd340f13c57
Zyeee-breaker <zyeeee33@gmail.com>
2024-10-14 08:17:55 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$host :'';
+$username:'';
+$password:'';
+$db:'';
+
+$db = mysqli_connect($server,$user,$password,$nama_database);
+
+if (!$db) {
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
